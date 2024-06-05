5b6c2e8e6e79964e5e95a493c6779f02fbcbc248
Liquid D. Raikov <109692107+raidenovich@users.noreply.github.com>
2024-06-05 17:04:24 +0300

    Update default.nix

--- disks/default.nix
+++ disks/default.nix
@@ -1,15 +1,9 @@
+{ device ? throw "Set this to your disk device, e.g. /dev/sda", ... }:
 {
-  device ? throw "Set this to your disk device, e.g. /dev/sda",
-  impermanence ? false,
-  lib,
-  swap ? false,
-  swapSize ? "4",
-  ...
-}: {
   disko = {
     devices = {
       disk = {
-        main = lib.mkIf impermanence {
+        main = {
           inherit device;
           type = "disk";
           content = {
@@ -34,67 +28,54 @@
                 size = "100%";
                 content = {
                   type = "zfs";
-                  pool = "zimpermanence";
-                };
-              };
-            };
-          };
-        };
-        zfs = lib.mkIf (impermanence == false) {
-          inherit device;
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              boot = {
-                name = "boot";
-                size = "1M";
-                type = "EF02";
-              };
-              esp = {
-                name = "ESP";
-                size = "64M";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
-              };
-              swap = lib.mkIf swap {
-                size = "${swapSize}G";
-                content = {
-                  type = "swap";
-                  resumeDevice = true;
-                };
-              };
-              root = {
-                size = "100%";
-                content = {
-                  type = "zfs";
                   pool = "zroot";
                 };
               };
             };
           };
         };
+        # deck = {
+        #   inherit device;
+        #   type = "disk";
+        #   content = {
+        #     type = "gpt";
+        #     partitions = {
+        #       boot = {
+        #         name = "boot";
+        #         size = "1M";
+        #         type = "EF02";
+        #       };
+        #       esp = {
+        #         name = "ESP";
+        #         size = "64M";
+        #         type = "EF00";
+        #         content = {
+        #           type = "filesystem";
+        #           format = "vfat";
+        #           mountpoint = "/boot";
+        #         };
+        #       };
+        #       root = {
+        #         size = "100%";
+        #         content = {
+        #           type = "zfs";
+        #           pool = "zroot2";
+        #         };
+        #       };
+        #     };
+        #   };
+        # };
       };
-      zpool = {
-        zroot = lib.mkIf impermanence {
+      zpool =  {
+        zroot = {
           type = "zpool";
           rootFsOptions = {
             canmount = "off";
           };
-
           datasets = {
             nix = {
               type = "zfs_fs";
               mountpoint = "/nix";
-              options.mountpoint = "legacy";
-            };
-            tmp = {
-              type = "zfs_fs";
-              mountpoint = "/tmp";
               options.mountpoint = "legacy";
             };
             persist = {
@@ -105,6 +86,11 @@
             persist-cache = {
               type = "zfs_fs";
               mountpoint = "/persist/cache";
+              options.mountpoint = "legacy";
+            };
+            tmp = {
+              type = "zfs_fs";
+              mountpoint = "/tmp";
               options.mountpoint = "legacy";
             };
             disks = {
